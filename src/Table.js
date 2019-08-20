2d4b5f82a73477d683b765f8620bc3eee04547d9
Aidarous Jibril <aidarousj7@gmail.com>
2019-08-20 22:01:27 +0200

    Update Table.js

--- src/Table.js
+++ src/Table.js
@@ -2,32 +2,37 @@ import React from "react";
 import "./Table.css";
 import ExpandRow from "./ExpandRow";
 
-function Table(props) {
+function Table(props) { 
+  //console.log(props)
   return (
     <table className="Table">
-      <tr>
-        <th>#{props.number}</th>
-        <th>{props.name ? props.name : "Name not available"}</th>
-        <th>{props.date}</th>
-      </tr>
+     <thead>
+        <tr>
+          <th>#{props.number}</th>
+          <th>{props.name ? props.name : "Name not available"}</th>
+          <th>{props.date}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr className="thead-dark">
+          <td>
+            <b>Start Number</b>
+          </td>
+          <td>
+            <b>Horse</b>
+          </td>
+          <td>
+            <b>Driver</b>
+          </td>
+        </tr>
 
-      <tr className="thead-dark">
-        <td>
-          <b>Start Number</b>
-        </td>
-        <td>
-          <b>Horse</b>
-        </td>
-        <td>
-          <b>Driver</b>
-        </td>
-      </tr>
-
-      {props.starts.map(start => (
-        <ExpandRow key={start.id} start={start} horse={start.horse} />
-      ))}
+        {props.starts.map(start => (
+          <ExpandRow key={start.number} start={start} horse={start.horse} />
+        ))}
+      </tbody>
     </table>
   );
 }
 
 export default Table;
+
