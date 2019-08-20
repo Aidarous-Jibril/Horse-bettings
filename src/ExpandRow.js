7063a3737eb1359aa574e6e27fe9c6eb8088880a
Aidarous Jibril <aidarousj7@gmail.com>
2019-08-20 22:02:09 +0200

    Update ExpandRow.js

--- src/ExpandRow.js
+++ src/ExpandRow.js
@@ -3,41 +3,33 @@ import "./Table.css";
 
 function StartRow({ start, horse }) {
   //console.log(start) console.log(horse)
-  //const trainer = horse.trainer
 
   //destructing horse.trainer props
   const trainer = horse.trainer;
   const [expanded, setExpanded] = useState(false);
 
-  //toggle to expand by using true or false of expanded value
+  //toggle to expand by using true or false of expanded boolean
   const toggleExpand = () => {
     expanded ? setExpanded(false) : setExpanded(true);
   };
 
   return (
     <React.Fragment>
-      <tr onClick={toggleExpand}>
+      <tr onClick={toggleExpand} >
         <td>#{start.number}</td>
         <td>{start.horse.name}</td>
-        <td>
-          {" "}
-          {start.driver.firstName} {start.driver.lastName}
-        </td>
+        <td>{start.driver.firstName} {start.driver.lastName}  </td>
       </tr>
       {expanded && (
-        <tr>
-          <td>
-            <p>
-              Trainer name:{" "}
-              <span style={getStyle}>
-                {" "}
-                {trainer.firstName} {trainer.lastName}
-              </span>{" "}
-            </p>
-            <p>
-              Horse name:{" "}
-              <span style={getStyle}> {horse.pedigree.father.name}</span>{" "}
-            </p>
+        <tr >
+          <td>            
+            <span style={getStyle}>Trainer firstname:</span> <br></br> {trainer.firstName} 
+          </td>
+          <td>            
+            <span style={getStyle}>Trainer lastname:</span> <br></br> {trainer.lastName}
+          </td>
+          <td>            
+            <span style={getStyle}>Horse father name:</span> <br></br> {horse.pedigree.father.name}    
           </td>
         </tr>
       )}
@@ -45,10 +37,10 @@ function StartRow({ start, horse }) {
   );
 }
 
+//style
 const getStyle = {
   fontFamily: "Arial",
-  fontSize: "16px",
-  color: "#8B0000",
-  paddingLeft: "5px"
+  fontWeight: "bolder",
+  color: "#8B0000"
 };
 export default StartRow;
