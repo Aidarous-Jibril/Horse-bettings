c572c6a77946fe0ad665e2d71eb6ff8f93be98e7
Aidarous <aidarousj7@gmail.com>
2022-01-13 03:39:29 +0100

    Updates

---
+++ precache-manifest.f6b0d4d473af2fbeb492d05756220f96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33dba523f8c21f05127c235b0ef9b074",
+    "url": "/jsr200-react/index.html"
+  },
+  {
+    "revision": "9d9baa9b517a6664e326",
+    "url": "/jsr200-react/static/css/main.d86c5bc6.chunk.css"
+  },
+  {
+    "revision": "6b2b163988d1e628302a",
+    "url": "/jsr200-react/static/js/2.f7506be0.chunk.js"
+  },
+  {
+    "revision": "9d9baa9b517a6664e326",
+    "url": "/jsr200-react/static/js/main.69c8341d.chunk.js"
+  },
+  {
+    "revision": "1a7892b804e0277c7396",
+    "url": "/jsr200-react/static/js/runtime~main.a468360a.js"
+  }
+]);
